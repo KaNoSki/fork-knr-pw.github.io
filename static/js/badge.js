71b52db10d09fa6468c409d1c1b109b19fddd448
LukPogo <lukaszpogo321@gmail.com>
2024-03-05 18:15:16 +0100

    poprawki do komentarzy

--- static/js/badge.js
+++ static/js/badge.js
@@ -8,22 +8,27 @@ function handleError(){
     elements.forEach(element => {
         element.className='haczyk_right haczyk_key_error';
     })
+    console.log("fetch_error");
+}
+
+function updateButtonState(element_id, current_state){
+    element_id.className='haczyk_right';
+    if (classMap.hasOwnProperty(current_state)) {
+        element_id.classList.add(classMap[current_state]);
+    } else {
+        element_id.classList.add("haczyk_key_error"); 
+    }
 }
 
 function getState(){
-    fetch('https://hangwatch.knr.edu.pl/hooks')
-    .then(odpowiedz=>odpowiedz.json())
+    fetch('hangwatch.knr.edu.pl/hooks')
+    .then(answer=>answer.json())
     .then(data => {
         for (const key in data) {
             if (data.hasOwnProperty(key)) {
-                const stan = data[key].state;
+                const state=data[key].state;
                 const element=document.getElementById(key);
-                element.className='haczyk_right';
-                if (classMap.hasOwnProperty(stan)) {
-                    element.classList.add(classMap[stan]);
-                } else {
-                    element.classList.add("haczyk_key_error"); 
-                }
+                updateButtonState(element,state);
             }
         }
     })
